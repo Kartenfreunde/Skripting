26741a5524428fb498492311984dfc97c5f82c1a
zimmermann-thomas <t.zimmermann96@t-online.de>
2021-11-20 15:19:27 +0100

    #18 Save only the important parts of the website

--- DataSourceWatcher/datasourcewatcher/__main__.py
+++ DataSourceWatcher/datasourcewatcher/__main__.py
@@ -1,13 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
+
 import urlsnapshot
 
 
-URLS = ['https://www.gdp-planetarium.org/planetarien/liste-der-planetarien/d-ost',
+GDP_URLS = ['https://www.gdp-planetarium.org/planetarien/liste-der-planetarien/d-ost',
         'https://www.gdp-planetarium.org/planetarien/liste-der-planetarien/d-nord',
         'https://www.gdp-planetarium.org/planetarien/liste-der-planetarien/d-mitte',
         'https://www.gdp-planetarium.org/planetarien/liste-der-planetarien/d-sued',
         'https://www.gdp-planetarium.org/planetarien/liste-der-planetarien/a-ch-fl-suedtirol']
 
 
+def _gdp_html(url: str):
+    return BeautifulSoup(requests.get(url).text, "html.parser").find("div", {"class": "content100"}).prettify()
+
+
 if __name__ == '__main__':
-    urlsnapshot.publish_snapshots({url: requests.get(url).text for url in URLS})
+    urlsnapshot.publish_snapshots({url: _gdp_html(url) for url in GDP_URLS})
